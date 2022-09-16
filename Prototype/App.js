5b9c266c35ac15b973b567f12a337f6da94a82a1
Haim Hong <4951hh@gmail.com>
2022-09-16 16:40:09 -0400

    Initial Prototype Commit

---
+++ Prototype/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is our prototype app</Text>
+      <Text>Papa</Text>
+      <Text>Nathanael</Text>
+      <Text>Caleb</Text>
+      <Text>Haim</Text>
+      <Text>Jacob</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
